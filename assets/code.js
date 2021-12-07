381e9de3e3ee1c5020aec9fb01dc15cf36bcbe86
St4d3ntG4zm4n <bwonder91@live.com>
2021-12-06 18:10:42 -0700

    going thru the question

--- assets/code.js
+++ assets/code.js
@@ -6,27 +6,58 @@
 $(".flashc").hide();
 $("#first").show();
 //intro
-$("#quizstart").click(function() {
+$("#startbtn").click(function() {
     $("#first").hide();
-    $("firstques").show();
+    $("#firstques").show();
 });
 //question1
 $("#firstques div").click(function() {
+    console.log("This is",$(this).attr("id"))
     $("#firstques").hide();
     $("#secondques").show();
+        if ($(this).attr("id") === "answerC") {
+            console.log ("correct answer")
+        } else {
+            console.log ("wrong answer")
+        }
+
 });
 //question2
 $("#secondques div").click(function() {
+    console.log("This is",$(this).attr("id"))
     $("#secondques").hide();
     $("#thirdques").show();
+    if ($(this).attr("id") === "answerA2") {
+        console.log ("correct answer")
+    } else {
+        console.log ("wrong answer")
+    }
 });
 //question3
 $("#thirdques div").click(function() {
+    console.log("This is",$(this).attr("id"))
     $("#thirdques").hide();
     $("#fourthques").show();
+    if ($(this).attr("id") === "answerA3") {
+        console.log ("correct answer")
+    } else {
+        console.log ("wrong answer")
+    }
 });
 //question4 last slide
 $("#fourthques div").click(function() {
+    console.log("This is",$(this).attr("id"))
     $("#fourthques").hide();
     $("#score").show();
+    if ($(this).attr("id") === "answerB4") {
+        console.log ("correct answer")
+    } else {
+        console.log ("wrong answer")
+    }
+});
+
+$( "#name" ).submit(function( event ) {
+    event.preventDefault();
+    //alert( "Handler for .submit() called." );
+    console.log($(this).children().val())
 });
